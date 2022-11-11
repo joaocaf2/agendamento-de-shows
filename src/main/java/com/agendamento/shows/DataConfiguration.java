2a39a95cfde10f59f3cb8ce6fcff9b29d4b9aea7
= <=>
2022-11-11 16:33:27 -0300

    Commit inicial

---
+++ src/main/java/com/agendamento/shows/DataConfiguration.java
@@ -0,0 +1,41 @@
+package com.agendamento.shows;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@Profile(value = "dev")
+public class DataConfiguration {
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = configuraDadosDoMySQL();
+		return dataSource;
+	}
+
+	private DriverManagerDataSource configuraDadosDoMySQL() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/shows");
+		dataSource.setUsername(System.getenv("userBd"));
+		dataSource.setPassword(System.getenv("passwdBd"));
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
