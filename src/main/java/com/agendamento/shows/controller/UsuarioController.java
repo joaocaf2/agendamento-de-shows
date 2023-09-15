7bd0aa5f5659a061694154cd5195b19ac46a9e90
Joao <jcaferreira9k@hotmail.com>
2023-09-14 23:22:51 -0300

    Adicionei o campo url do poster e comecei a implementar o cadastro de usuario

---
+++ src/main/java/com/agendamento/shows/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+package com.agendamento.shows.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("usuario")
+public class UsuarioController {
+
+
+}
